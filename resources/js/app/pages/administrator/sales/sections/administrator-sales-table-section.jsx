48563c9d9afc89d30279ef1e918c7bd45ccbec86
marlou <gb.bakeshop.app@gmail.com>
2024-03-16 18:38:24 +0800

    added new build

--- resources/js/app/pages/administrator/sales/sections/administrator-sales-table-section.jsx
+++ resources/js/app/pages/administrator/sales/sections/administrator-sales-table-section.jsx
@@ -195,13 +195,13 @@ export default function AdministratorSalesTableSection() {
 
                             <div className="mt-6 sm:flex sm:items-center sm:justify-between ">
                                 <div className="text-sm text-gray-500 ">
-                                    Page <span className="font-medium text-gray-700  text-gray-100">1 of 10</span>
+                                    Page <span className="font-medium text-gray-700 ">1 of 10</span>
                                 </div>
 
                                 <div className="flex items-center mt-4 gap-x-4 sm:mt-0">
                                     <button
                                         onClick={backPage}
-                                        className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100  bg-gray-900  text-gray-200    hover:bg-gray-800">
+                                        className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 ">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 rtl:-scale-x-100">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                                         </svg>
@@ -213,7 +213,7 @@ export default function AdministratorSalesTableSection() {
 
                                     <button
                                         onClick={nextPage}
-                                        className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100  bg-gray-900  text-gray-200    hover:bg-gray-800">
+                                        className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 ">
                                         <span>
                                             Next
                                         </span>
